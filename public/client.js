94979cdc15ac6e2a04842e108f51c7b41e09e9c6
Mauricio Lahitte <Mauricio_Lahitte@intranet.itresources>
2021-06-17 17:32:38 -0300

    style added

--- public/client.js
+++ public/client.js
@@ -3,15 +3,27 @@ const client = io();
 let txtKey = document.getElementById('txtkey');
 let txtValue = document.getElementById('txtvalue');
 let btnSend = document.getElementById('send');
+let divResult = document.getElementById('result-container');
+let txtResult = document.getElementById('textResult');
 
 btnSend.addEventListener('click', function () {
     if (txtKey.value == "") {
         console.log('key must be not empty');
+        divResult.style.visibility = "visible"; 
+        txtResult.innerHTML = "<b>The key must not be empty </b>"
+    }else{
+        client.emit('new:keyPair', {
+            key: txtKey.value,
+            value: txtValue.value
+        });
+        txtKey.value = "",
+        txtValue.value = ""    
+
+        divResult.style.visibility = "visible"; 
+        txtResult.innerHTML = "<b>Stored!</b>"
     }
-    client.emit('new:keyPair', {
-        key: txtKey.value,
-        value: txtValue.value
-    });
-    txtKey.value = "",
-    txtValue.value = ""    
+
+    setTimeout(() => {
+        divResult.style.visibility = "hidden"; 
+    }, 3000);
 })
